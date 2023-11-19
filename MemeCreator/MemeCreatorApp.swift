faa530c80c1d05a21c9c951329feb56a7d86bf8f
Eric <ebagnaro@gmail.com>
2023-11-19 16:20:53 +0100

    Initial Commit

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by Eric on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
